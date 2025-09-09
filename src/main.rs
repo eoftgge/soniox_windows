a1148b58f746aa790d12bcf9bb179a99a1654691
consteremnaresinf (eoftgge) <sandaar.ykt@gmail.com>
2025-09-09 21:59:17 +0900

    remove unnecessary types

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ use std::error::Error;
 use std::thread;
 
 use crossbeam_channel::unbounded;
-use hound::{SampleFormat, WavSpec, WavWriter};
 use soniox_windows::stream::start_capture_audio;
-use wasapi::{Direction, get_default_device, initialize_mta};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (tx, rx) = unbounded::<Vec<u8>>();
     thread::spawn(move || start_capture_audio(tx));
-    
+
     Ok(())
 }
