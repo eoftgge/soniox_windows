88c1007df34e631bc0d747ab4ac54eec3f155da1
consteremnaresinf (eoftgge) <sandaar.ykt@gmail.com>
2025-09-09 22:03:57 +0900

    add public privacy for all types

--- src/types.rs
+++ src/types.rs
@@ -1,51 +1,50 @@
-use std::sync::Arc;
 use serde::{Deserialize, Serialize};
 
 pub type AudioSample = Vec<u8>;
 
 #[derive(Debug, Deserialize, Default)]
 pub struct SonioxTranslationObject {
-    r#type: String,
-    language_a: String,
-    language_b: String,
+    pub r#type: String,
+    pub language_a: String,
+    pub language_b: String,
 }
 
 #[derive(Debug, Deserialize, Default)]
 #[serde(default)]
 pub struct SonioxTranscriptionRequest {
-    api_key: String,
-    model: String,
-    audio_format: String,
-    num_channels: Option<u32>, // required for raw audio
-    sample_rate: Option<u32>, // required for raw audio
-    language_hints: Vec<String>, // required
-    context: Option<String>,
-    enable_speaker_diarization: Option<bool>,
-    enable_language_identification: Option<bool>,
-    enable_non_final_tokens: Option<bool>,
-    enable_endpoint_detection: Option<bool>,
-    client_reference_id: Option<String>,
-    translation: Option<SonioxTranslationObject>,
+    pub api_key: String,
+    pub model: String,
+    pub audio_format: String,
+    pub num_channels: Option<u32>, // required for raw audio
+    pub sample_rate: Option<u32>, // required for raw audio
+    pub language_hints: Vec<String>, // required
+    pub context: Option<String>,
+    pub enable_speaker_diarization: Option<bool>,
+    pub enable_language_identification: Option<bool>,
+    pub enable_non_final_tokens: Option<bool>,
+    pub enable_endpoint_detection: Option<bool>,
+    pub client_reference_id: Option<String>,
+    pub translation: Option<SonioxTranslationObject>,
 }
 
 #[derive(Debug, Serialize, Default)]
 #[serde(default)]
 pub struct SonioxTranscriptionToken {
-    text: String,
-    start_ms: Option<u32>,
-    end_ms: Option<u32>,
-    confidence: u32,
-    is_final: bool,
-    speaker: Option<String>,
-    language: Option<String>,
-    source_language: Option<String>,
+    pub text: String,
+    pub start_ms: Option<u32>,
+    pub end_ms: Option<u32>,
+    pub confidence: u32,
+    pub is_final: bool,
+    pub speaker: Option<String>,
+    pub language: Option<String>,
+    pub source_language: Option<String>,
 }
 
 #[derive(Debug, Serialize, Default)]
 #[serde(default)]
 pub struct SonioxTranscriptionResponse {
-    tokens: Vec<SonioxTranscriptionToken>,
-    final_audio_proc_ms: u32,
-    total_audio_proc_ms: u32,
-    finished: Option<bool>,
+    pub tokens: Vec<SonioxTranscriptionToken>,
+    pub final_audio_proc_ms: u32,
+    pub total_audio_proc_ms: u32,
+    pub finished: Option<bool>,
 }
