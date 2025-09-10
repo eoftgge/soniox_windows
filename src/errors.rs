7b143402dbe0dc56e330bff65a68c49b0138cf3a
consteremnaresinf (eoftgge) <sandaar.ykt@gmail.com>
2025-09-10 11:35:15 +0900

    modify error handler

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use std::env;
 use wasapi::WasapiError;
 
 #[derive(thiserror::Error, Debug)]
@@ -8,6 +9,10 @@ pub enum SonioxWindowsErrors {
     Websocket(#[from] tungstenite::Error),
     #[error("{0}")]
     Json(#[from] serde_json::Error),
+    #[error("{0}")]
+    Egui(#[from] eframe::Error),
+    #[error("{0}")]
+    Env(#[from] env::VarError),
     #[error("{0}")]
     Internal(&'static str)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 #![windows_subsystem = "windows"]
-use std::error::Error;
 use std::thread;
 
 use crossbeam_channel::unbounded;
-use egui::ViewportBuilder;
+use eframe::egui::ViewportBuilder;
+use soniox_windows::errors::SonioxWindowsErrors;
 use soniox_windows::gui::SubtitlesApp;
 use soniox_windows::soniox::start_soniox_stream;
 use soniox_windows::stream::start_capture_audio;
 use soniox_windows::types::AudioSample;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), SonioxWindowsErrors> {
     let api_key = std::env::var("SONIOX_APIKEY")?;
     let (tx, rx) = unbounded::<AudioSample>();
     let (tx_text, rx_text) = unbounded::<String>();
     let app = SubtitlesApp::new(rx_text);
-    thread::spawn(move || start_capture_audio(tx));
-    tokio::spawn(start_soniox_stream(rx, api_key, tx_text));
+    thread::spawn(move || {
+        if let Err(err) = start_capture_audio(tx) {
+            log::error!("{}", err);
+        }
+    });
+    tokio::spawn(async move {
+        if let Err(err) = start_soniox_stream(rx, api_key, tx_text).await {
+            log::error!("{}", err);
+        }
+    });
 
     let native_options = eframe::NativeOptions {
         viewport: ViewportBuilder::default()
@@ -31,10 +39,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         ..Default::default()
     };
 
-    let _ = eframe::run_native(
+    eframe::run_native(
         "Subtitle Live",
         native_options,
         Box::new(move |_| Ok(Box::new(app))),
-    );
+    )?;
     Ok(())
 }
