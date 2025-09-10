f6e7c3f5f715d067f91d0ba9152d718b49ef500f
consteremnaresinf (eoftgge) <sandaar.ykt@gmail.com>
2025-09-10 09:09:50 +0900

    add clickable over window

--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,33 @@
 use crossbeam_channel::Receiver;
-use eframe::{App, Frame, egui};
-use std::time::Duration;
 use eframe::epaint::Color32;
+use eframe::{egui, App, Frame};
 use egui::Visuals;
+use std::time::Duration;
+use raw_window_handle::HasWindowHandle;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowLongW, SetWindowLongW, GWL_EXSTYLE, WS_EX_LAYERED, WS_EX_TRANSPARENT};
+
+fn make_window_click_through(hwnd: HWND) {
+    unsafe {
+        let ex_style = GetWindowLongW(hwnd, GWL_EXSTYLE);
+        SetWindowLongW(hwnd, GWL_EXSTYLE, ex_style | WS_EX_LAYERED.0 as i32 | WS_EX_TRANSPARENT.0 as i32);
+    }
+}
+
+fn set_click_through(frame: &eframe::Frame) {
+    use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+
+    match frame.window_handle() {
+        Ok(handle) => {
+            let raw = handle.as_raw();
+            if let RawWindowHandle::Win32(win32) = raw {
+                let hwnd = HWND(win32.hwnd.get() as *mut _);
+                make_window_click_through(hwnd);
+            }
+        }
+        _ => {}
+    }
+}
 
 pub struct SubtitlesApp {
     rx: Receiver<String>,
@@ -19,7 +44,8 @@ impl SubtitlesApp {
 }
 
 impl App for SubtitlesApp {
-    fn update(&mut self, ctx: &egui::Context, _: &mut Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        set_click_through(frame);
         while let Ok(new_text) = self.rx.try_recv() {
             self.text = new_text;
         }
