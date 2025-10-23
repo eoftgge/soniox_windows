42f4a354dcbfd733969062a30affb3102287ad91
setkg <sandaar.ykt@gmail.com>
2025-10-23 17:28:33 +0900

    remove function `create_request` to new file and add sleeping in listen websocket

--- src/soniox/mod.rs
+++ src/soniox/mod.rs
@@ -1,2 +1,6 @@
 pub(crate) mod render;
+pub(crate) mod request;
 pub mod stream;
+
+pub const URL: &str = "wss://stt-rt.soniox.com/transcribe-websocket";
+pub const MODEL: &str = "stt-rt-v3";
--- src/soniox/stream.rs
+++ src/soniox/stream.rs
@@ -1,48 +1,19 @@
+use std::thread::sleep;
 use crate::errors::SonioxWindowsErrors;
 use crate::soniox::render::render_transcription;
 use crate::types::audio::{AudioMessage, AudioSubtitle};
 use crate::types::settings::SettingsApp;
 use crate::types::soniox::{
-    SonioxTranscriptionRequest, SonioxTranscriptionResponse, SonioxTranslationObject,
+    SonioxTranscriptionResponse, SonioxTranslationObject,
 };
+use crate::soniox::URL;
 use futures_util::{SinkExt, StreamExt};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio_tungstenite::connect_async;
 use tungstenite::client::IntoClientRequest;
 use tungstenite::{Bytes, Message, Utf8Bytes};
-use wasapi::{Direction, get_default_device, initialize_mta};
-
-const URL: &str = "wss://stt-rt.soniox.com/transcribe-websocket";
-const MODEL: &str = "stt-rt-v3";
-
-fn create_request(settings: SettingsApp) -> SonioxTranscriptionRequest {
-    initialize_mta().ok().unwrap();
-    let device = get_default_device(&Direction::Render).ok().unwrap();
-    let audio_client = device.get_iaudioclient().ok().unwrap();
-    let format = audio_client.get_mixformat().ok().unwrap();
-    let sample_rate = format.get_samplespersec();
-    let channels = format.get_nchannels();
-    let mut request = SonioxTranscriptionRequest {
-        api_key: settings.api_key,
-        model: MODEL.into(),
-        audio_format: "pcm_s16le".into(),
-        sample_rate: Some(sample_rate),
-        num_channels: Some(channels as u32),
-        context: Some(settings.context),
-        language_hints: settings.language_hints,
-        enable_speaker_diarization: Some(settings.enable_speakers),
-        ..Default::default()
-    };
-    if settings.enable_translate {
-        request.translation = Some(SonioxTranslationObject {
-            r#type: "one_way".into(),
-            target_language: Some(settings.target_language),
-            ..Default::default()
-        });
-    }
-
-    request
-}
+use wasapi::{get_default_device, initialize_mta};
+use crate::soniox::request::create_request;
 
 async fn listen_soniox_stream(
     bytes: Vec<u8>,
@@ -65,6 +36,7 @@ async fn listen_soniox_stream(
                     let subtitle = render_transcription(&response);
                     let _ = tx_subs.send(subtitle);
                 }
+                tokio::time::sleep(std::time::Duration::from_millis(100)).await;
             }
             <Result<(), SonioxWindowsErrors>>::Ok(())
         };
