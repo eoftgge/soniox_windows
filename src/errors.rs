479c7a5fd6afd41cd2a7b5f64111390dee964aeb
eoftgge <sandaar.ykt@gmail.com>
2025-12-07 17:48:01 +0900

    refactor: mark error

--- src/errors.rs
+++ src/errors.rs
@@ -4,24 +4,22 @@ use wasapi::WasapiError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum SonioxWindowsErrors {
-    #[error("{0}")]
+    #[error("Error in audio: {0}")]
     Wasapi(#[from] WasapiError),
-    #[error("{0}")]
+    #[error("Error in WEB: {0}")]
     Websocket(#[from] tungstenite::Error),
-    #[error("{0}")]
+    #[error("Error in JSON: {0}")]
     Json(#[from] serde_json::Error),
-    #[error("{0}")]
-    Egui(#[from] eframe::Error),
-    #[error("{0}")]
-    Env(#[from] env::VarError),
-    #[error("{0}")]
+    #[error("Error in Graphics: {0}")]
+    Graphics(#[from] eframe::Error),
+    #[error("Error in config: {0}")]
     Config(#[from] config::ConfigError),
-    #[error("{0}")]
+    #[error("Error in IO: {0}")]
     Io(#[from] std::io::Error),
-    #[error("{0}")]
+    #[error("Error in log: {0}")]
     LoggingRuntime(#[from] ConfigErrors),
-    #[error("{0}")]
+    #[error("Error in string UTF-8: {0}")]
     Utf8(#[from] std::str::Utf8Error),
-    #[error("{0}")]
+    #[error("Internal error: {0}")]
     Internal(&'static str),
 }
