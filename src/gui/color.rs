09f4ba367ba97715f8864fa1b15413ef0a303aa7
eoftgge <sandaar.ykt@gmail.com>
2026-02-04 22:02:11 +0900

    feat: smartest get interim color

--- src/gui/color.rs
+++ src/gui/color.rs
@@ -1,11 +1,16 @@
 use eframe::egui::Color32;
+use eframe::epaint::Hsva;
 
 pub(crate) fn get_interim_color(main_color: Color32) -> Color32 {
-    if main_color == Color32::WHITE {
-        Color32::from_gray(180)
-    } else if main_color == Color32::YELLOW {
-        Color32::from_rgb(200, 180, 100)
+    let mut hsva = Hsva::from(main_color);
+
+    if hsva.s < 0.1 {
+        hsva.v = 0.65;
     } else {
-        Color32::from_white_alpha(150)
+        hsva.s *= 0.4;
+        hsva.v = 1.0;
     }
+
+    hsva.a = 1.0;
+    Color32::from(hsva)
 }
