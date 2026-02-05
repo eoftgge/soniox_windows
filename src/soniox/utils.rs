bd9b9367a0e26c4416968dbfb0463547698f19d3
eoftgge <sandaar.ykt@gmail.com>
2026-02-05 22:24:34 +0900

    style: rusfmt

--- src/soniox/utils.rs
+++ src/soniox/utils.rs
@@ -2,7 +2,5 @@ pub fn convert_audio_chunk(input: &[f32], output: &mut Vec<i16>) {
     output.clear();
     const SCALE: f32 = i16::MAX as f32;
 
-    output.extend(input.iter().map(|&s| {
-        (s.clamp(-1.0, 1.0) * SCALE) as i16
-    }));
+    output.extend(input.iter().map(|&s| (s.clamp(-1.0, 1.0) * SCALE) as i16));
 }
