d327347bd3f39338e0193feefc9af9335748436d
eoftgge <sandaar.ykt@gmail.com>
2026-02-06 17:11:39 +0900

    fix: logging file

--- src/lib.rs
+++ src/lib.rs
@@ -18,19 +18,13 @@ fn setup_tracing(level: LevelFilter) -> tracing_appender::non_blocking::WorkerGu
     let file_appender = tracing_appender::rolling::daily("logs", "soniox.log");
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_writer(std::io::stdout)
-                .with_ansi(true),
-        )
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_writer(non_blocking)
-                .with_ansi(false)
-                .with_target(false),
-        )
-        .with(level)
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_max_level(level)
+        .with_ansi(false)
+        .with_thread_names(true)
+        .with_file(true)
+        .with_line_number(true)
         .init();
 
     guard
